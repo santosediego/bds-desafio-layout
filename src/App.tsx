2f0f27305c84a9f414df309eee9ef7ed10972a5f
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-11-14 14:03:53 -0300

    Navbar component , component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./assets/styles/custom.scss";
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <p>Hello World</p>
+    <Navbar />
   );
 }
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+function Navbar() {
+    return (
+        <nav className='navbar navbar-expand-md navbar-dark bg-primary main-nav'>
+            <div className='container-fluid'>
+                <a href="link">
+                    <h4>Carros Top</h4>
+                </a>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#carros-top-navbar"
+                    aria-controls="carros-top-navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className='collapse navbar-collapse' id='carros-top-navbar'>
+                    <ul className='navbar-nav offset-md-9 main-menu'>
+                        <li>
+                            <a href="link" className='active'>Home</a>
+                        </li>
+                        <li>
+                            <a href="link">Catálogo</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
